9d0066bd24c6771d94eb58ba5b0491d18c1f7c73
jaspreetdhanjan <jaspreetdhanjan@gmail.com>
2017-02-06 18:40:30 +0000

    Added doc

--- src/com/basementstudios/tag/component/Component.java
+++ src/com/basementstudios/tag/component/Component.java
@@ -1,5 +1,13 @@
 package com.basementstudios.tag.component;
 
+/**
+ * A component designed for additional features for entities.
+ * 
+ * Used to center all additional features into one singular class instead of repeating code.
+ * 
+ * @author Jaspreet Dhanjan
+ */
+
 public interface Component {
 	public void tick();
 }
