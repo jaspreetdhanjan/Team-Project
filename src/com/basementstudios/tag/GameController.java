3eeb045cede2375c54430a2740565e3f544b3eed
yarbsemaj <yarbsemaj@gmail.com>
2017-03-14 23:45:46 +0000

    All chara now get a turn at the start

--- src/com/basementstudios/tag/GameController.java
+++ src/com/basementstudios/tag/GameController.java
@@ -98,7 +98,7 @@ public class GameController {
         maxSpd = playerController.getMaxSpeed();
         if (enemyController.getMaxSpeed() > maxSpd)
             maxSpd = enemyController.getMaxSpeed();
-        turn++;
+        maxSpd++;
 
         getNext();
     }
